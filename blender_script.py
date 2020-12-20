9d7202e5570fbf63211ebb4bf4d98d50939cbfcc
Veenxz <7677356+veenxz@user.noreply.gitee.com>
2020-12-20 14:44:43 +0800

    first commit

---
+++ blender_script.py
@@ -0,0 +1,87 @@
+import bpy
+
+
+def clear_all():
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete(use_global=False, confirm=False)
+
+
+def bool(obj, obj_c, type):
+    bool = obj.modifiers.new(name='Bool', type='BOOLEAN')
+    bool.operation = type
+    bool.object = obj_c
+
+
+clear_all()
+
+sm = 256
+r = [2.0, 0.2]
+sc = [1.8, 1.5, 2, 1.4, 1]
+h = 0.5
+z = 0.5 * sc[3] * r[0] + h
+
+bpy.ops.mesh.primitive_uv_sphere_add(segments=sm,
+                                     ring_count=0.5 * sm,
+                                     radius=r[0],
+                                     enter_editmode=False,
+                                     align='WORLD',
+                                     location=(0, 0, z),
+                                     scale=(sc[0], sc[1], sc[2]))
+
+bpy.ops.mesh.primitive_uv_sphere_add(segments=sm,
+                                     ring_count=0.5 * sm,
+                                     radius=r[0],
+                                     enter_editmode=False,
+                                     align='WORLD',
+                                     location=(0, 0, z),
+                                     scale=(sc[0], sc[1], sc[3]))
+
+bpy.ops.mesh.primitive_cylinder_add(vertices=0.5 * sm,
+                                    radius=r[1],
+                                    depth=1.2 * h,
+                                    end_fill_type='NGON',
+                                    calc_uvs=True,
+                                    enter_editmode=False,
+                                    align='WORLD',
+                                    location=(0.0, 0.0, 0.6 * h),
+                                    rotation=(0.0, 0.0, 0.0),
+                                    scale=(sc[4], sc[4], sc[4]))
+
+bpy.ops.mesh.primitive_plane_add(size=max(r) * max(sc),
+                                 calc_uvs=True,
+                                 enter_editmode=False,
+                                 align='WORLD',
+                                 location=(0, 0, z),
+                                 scale=(1, 1, 1))
+
+obj = bpy.data.objects
+sphere0 = obj['Sphere']
+sphere1 = obj['Sphere.001']
+plane = obj['Plane']
+cylinder = obj['Cylinder']
+
+bool(sphere0, plane, 'DIFFERENCE')
+bool(sphere1, sphere0, 'DIFFERENCE')
+bool(sphere1, sphere0, 'UNION')
+bool(sphere1, cylinder, 'UNION')
+
+sphere1.name = 'Tree'
+plane.select_set(False)
+obj['Tree'].select_set(True)
+
+bpy.ops.export_mesh.stl(filepath='./constant/triSurface/Tree.stl',
+                        check_existing=True,
+                        filter_glob='*.stl',
+                        use_selection=True,
+                        global_scale=1.0,
+                        use_scene_unit=False,
+                        ascii=False,
+                        use_mesh_modifiers=True,
+                        batch_mode='OFF',
+                        axis_forward='Y',
+                        axis_up='Z')
+
+#clear_all()
+#bpy.ops.import_mesh.stl(filepath='Tree.stl')
+
+print('Geometry Generation Finshed!')
---
+++ snapmesh.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Libraries
+
+# Function definitions
+def headerLines(cl, loc, obj, con="convertToMeters 1;"):
+    h = [
+        "/*--------------------------------*- C++ -*----------------------------------*|",
+        "| =========                 |                                                 |",
+        "| \\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |",
+        "|  \\\    /   O peration     | Website:  https://openfoam.org                  |",
+        "|   \\\  /    A nd           | Version:  7                                     |",
+        "|    \\\/     M anipulation  |                                                 |",
+        "\*---------------------------------------------------------------------------*/",
+        "FoamFile", "{", "    version     2.0;", "    format      ascii;",
+        "    class       {};", '    location    "{}";', "    object      {};",
+        "}",
+        "// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //",
+        "", "{}", ""
+    ]
+
+    h[11] = h[11].format(cl)
+    h[12] = h[12].format(loc)
+    h[13] = h[13].format(obj)
+    h[17] = h[17].format(con)
+    return h
+
+bl = '    '
+loc = "system"
+cl = "dictionary"
+obj = "snappyHexMeshDict"
+
